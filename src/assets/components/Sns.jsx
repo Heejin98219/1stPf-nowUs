415b3be84b6ea7ab66d86659a6ccdba198b04d45
whgmlwls0219 <whgmlwls0219@naver.com>
2025-10-28 19:44:21 +0900

    sns 아이콘 sns 사이트로 이동하기 기능 완료

--- src/assets/components/Sns.jsx
+++ src/assets/components/Sns.jsx
@@ -1,40 +1,68 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faFacebook } from "@fortawesome/free-brands-svg-icons";
-import { faInstagram } from "@fortawesome/free-brands-svg-icons";
-import { faTwitter } from "@fortawesome/free-brands-svg-icons";
-import { faYoutube } from "@fortawesome/free-brands-svg-icons";
-import { faThreads } from "@fortawesome/free-brands-svg-icons";
+import {
+  faFacebook,
+  faInstagram,
+  faTwitter,
+  faYoutube,
+  faThreads,
+} from "@fortawesome/free-brands-svg-icons";
 
 const Sns = () => {
   return (
-    <>
-      {/* sns 입니다 */}
-      <div className="sns-Div">
-        <ul className="snsIcons">
-          <li>
-            <FontAwesomeIcon icon={faFacebook} size="2xl" className="snsIcon" />
-          </li>
-          <li>
+    <div className="sns-Div">
+      <ul className="snsIcons">
+        <li>
+          <a
+            href="https://twitter.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FontAwesomeIcon icon={faTwitter} size="2xl" className="snsIcon" />
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://www.instagram.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             <FontAwesomeIcon
               icon={faInstagram}
               size="2xl"
               className="snsIcon"
             />
-          </li>
-          <li>
-            <FontAwesomeIcon icon={faTwitter} size="2xl" className="snsIcon" />
-          </li>
-          <li>
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://www.youtube.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             <FontAwesomeIcon icon={faYoutube} size="2xl" className="snsIcon" />
-          </li>
-          <li>
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://www.facebook.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FontAwesomeIcon icon={faFacebook} size="2xl" className="snsIcon" />
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://www.threads.net"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             <FontAwesomeIcon icon={faThreads} size="2xl" className="snsIcon" />
-          </li>
-        </ul>
-      </div>
-    </>
+          </a>
+        </li>
+      </ul>
+    </div>
   );
 };
 
 export default Sns;
-``;
