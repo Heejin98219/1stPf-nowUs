cd73ed6cc74fbafe87ffa6a3e0ab4f48df22183b
whgmlwls0219 <whgmlwls0219@naver.com>
2025-10-29 21:17:19 +0900

    Grid 디자인 수정(사진 삽입, 닉네임 추가, 멘트 수정)

--- src/assets/components/DogsDay.jsx
+++ src/assets/components/DogsDay.jsx
@@ -5,20 +5,28 @@ const DogsDay = () => {
       <div className="dogsDay">
         <img src="./public/image/Dog.jpg" className="dogImg" alt="이미지"></img>
         <div className="ment">
-          <span className="date">3월 23일</span>
-          <span style={{ fontFamily: "inherit", fontSize: "20px" }}>은</span>
-          <br />
-          <span className="day">강아지의 날&nbsp;</span>
-          <span style={{ fontFamily: "inherit", fontSize: "20px" }}>
-            입니다
-          </span>
-          <br />
-          <span className="promise">
-            지금, 우리는 모든 동물들이 행복할 수 있는 그 날까지
+          <div className="tellDogsDay">
+            <span className="date">3월 23일</span>
+            <span style={{ fontFamily: "inherit", fontSize: "20px" }}>은</span>
             <br />
-            계속 함께하겠습니다
-            <br />
-          </span>
+            <span className="day">강아지의 날&nbsp;</span>
+            <span style={{ fontFamily: "inherit", fontSize: "20px" }}>
+              입니다
+            </span>
+          </div>
+
+          <br />
+          <div className="promiseMent">
+            <span className="promise">
+              보호소 '지금, 우리'는
+              <br />
+              모든 동물들이 행복할 수 있는 그 날까지
+              <br />
+              계속 함께하겠습니다.
+              <br />
+            </span>
+          </div>
+
           <ul className="together">
             <li>&#35;함께해요</li>
             <li>&#35;동물</li>
--- src/assets/components/Review.jsx
+++ src/assets/components/Review.jsx
@@ -3,55 +3,121 @@ const reviews = [
     id: 1,
     img: "https://www.zooseyo.or.kr/dog_sale/photo_free/202402/1708892155_69668500.jpg",
     name: "kyuzizi",
-    text: "작은 거 하나에도 좋아하는 모습 볼 때마다 마음이 따듯해져요 ㅎㅎ",
+    text: "작은 거 하나에도 좋아하는 모습 보니까 마음이 너무 따듯해져요..",
   },
   {
     id: 2,
     img: "https://cdn.imweb.me/upload/S20241015a7b4a02f012a7/361159dad5377.png",
-    name: "베이니플",
-    text: "덕분에 집이 집이 더 따뜻해 졌어요ㅠㅠ",
+    name: "whdmsehd0830",
+    text: "덕분에 집이 더 따뜻해졌어요 ㅎㅎ",
   },
   {
     id: 3,
     img: "https://changwon.go.kr/animal/atcfile/25550712550102760.jpg",
-    name: "미족",
-    text: "항상 제게 위안이 되고 있어요ㅎㅎ ",
+    name: "jh1229",
+    text: "항상 제게 위안이 되고 있어요ㅎㅎ",
   },
   {
     id: 4,
     img: "https://mediahub.seoul.go.kr/wp-content/uploads/2020/02/2f6ae5e9b4c59861183ae1cb80d5722d.jpg",
-    name: "Kuzi",
+    name: "byunsw614",
     text: "한 걸음 걷는 것만 봐도 미소가 절로 나요 ㅎㅎ",
   },
   {
     id: 5,
     img: "https://cdn.imweb.me/upload/S201806265b31e9f9b9a03/167b351986d5a.jpg",
-    name: "제너럴지크",
+    name: "junlee",
     text: "처음보는 순간 운명이라고 생각했어요 ㅋㅋ",
   },
   {
     id: 6,
     img: "https://cdn.imweb.me/upload/S201711105a050488bde89/a7475a6f010e1.jpg",
-    name: "gitMERGE",
-    text: "금수저 안 부러워요 ㅋㅋ",
+    name: "yeonseo3010",
+    text: "금수저 안 부럽네요",
   },
   {
     id: 7,
     img: "https://flexible.img.hani.co.kr/flexible/normal/970/728/imgdb/original/2020/1104/20201104503342.jpg",
-    name: "나의 지구",
+    name: "sy1804",
     text: "하루 하루가 소중해 졌어요ㅠㅠ",
   },
   {
     id: 8,
     img: "https://gnews.gg.go.kr/OP_UPDATA/UP_DATA/_FILEZ/202404/20240426012358819526104.jpg",
-    name: "INSPRT",
+    name: "",
     text: "눈 마주칠 때마다 녹아내릴 거 같아요ㅠㅠ",
   },
   {
     id: 9,
-    img: "https://cdn.imweb.me/upload/S20241015a7b4a02f012a7/91020a5edcb71.png",
-    name: "N1zoral",
-    text: "입양, 정말 잘 한 거 같아요!!",
+    img: "https://cdn.dailyvet.co.kr/wp-content/uploads/2024/12/05205858/20241205ceva_experts2.jpg",
+    name: "",
+    text: "입양, 정말 잘 한 거 같아요!",
+  },
+  {
+    id: 10,
+    img: "https://www.biteme.co.kr/blog/wp-content/uploads/2025/03/grant-durr-GZXf4kGrqEA-unsplash.jpg",
+    name: "eungdongee",
+    text: "덕분에 더 열심히 살게 돼요 : )",
+  },
+  {
+    id: 11,
+    img: "https://images.pet-friends.co.kr/storage/pet_friends/product/id/4/d/1/2/e/a/b/4d12eab979f300e89d4320282b5493b8/10000/8bd888987702079638cb595ac42e6349.jpg",
+    name: "lmyungsooisl",
+    text: "인생은 강아지 키우기 전과 후로 나뉘네여",
+  },
+  {
+    id: 12,
+    img: "https://static.fitpetcdn.com/prod/images/product/1000037597/%EB%98%91%EC%82%AC%EC%84%B8%EC%9A%94_%EB%98%91_%EB%91%90%EA%B1%B4_%EB%AA%A8%EC%9E%90_1_uyudnn.jpg",
+    name: "namoo",
+    text: "눈에 넣어도 안 아프다는 게 뭔지 알겠어요",
+  },
+  {
+    id: 13,
+    img: "https://cdn.crowdpic.net/detail-thumb/thumb_d_93D9CB6D3B0C5421E5D541FB441B7BAB.jpg",
+    name: "lastRomeo17",
+    text: "진작 입양할 걸 그랬어여",
+  },
+  {
+    id: 14,
+    img: "https://ichef.bbci.co.uk/ace/ws/640/cpsprodpb/AD6E/production/_104889344_kitten.jpg.webp",
+    name: "13egin",
+    text: "어떻게 이런 복덩이가 굴러왔을까요..?!",
+  },
+  {
+    id: 15,
+    img: "https://image.dongascience.com/Photo/2022/05/216542246ab763e7ddaa6c5b2d28b31d.jpg",
+    name: "1nf1n1t3",
+    text: "적응도 못했는데.. 이제 친구도 사귀어요",
+  },
+  {
+    id: 16,
+    img: "https://img2.daumcdn.net/thumb/R658x0.q70/?fname=https://t1.daumcdn.net/news/202507/23/newsay/20250723140003290pgal.jpg",
+    name: "whgmlwls1221",
+    text: "우리집 두 모녀들, 잘 살겠습니다",
+  },
+  {
+    id: 17,
+    img: "https://image.dongascience.com/Photo/2020/11/7ae755d21462741dc15d14a048d981f8.jpg",
+    name: "rkddnjsxor11",
+    text: "하는 행동들이 어쩜 이렇게 다 사랑스러울까요",
+  },
+  {
+    id: 18,
+    img: "https://cdn.dailyvet.co.kr/wp-content/uploads/2024/05/15231647/20240515ceva_experts4.jpg",
+    name: "rkddnjsxor56",
+    text: "눈 마주치면 심장이 쿵! 하고 내려앉는 거 같아요 (>ㅁ<)",
+  },
+  {
+    id: 19,
+    img: "https://cdn.korearailroad.kr/news/photo/202503/2285_3028_626.jpg",
+    name: "sunny",
+    text: "애인 없어도 전 연분홍색 빛 안에 있어요 ㅎㅎㅎㅎㅎㅎ",
+  },
+  {
+    id: 20,
+    img: "https://www.biteme.co.kr/blog/wp-content/uploads/2024/12/%E1%84%80%E1%85%A1%E1%86%BC%E1%84%8B%E1%85%A1%E1%84%8C%E1%85%B5-%E1%84%8B%E1%85%B5%E1%86%B8%E1%84%8B%E1%85%A3%E1%86%BC-%E1%84%80%E1%85%A1%E1%86%BC%E1%84%8B%E1%85%A1%E1%84%8C%E1%85%B5-%E1%84%8F%E1%85%B5%E1%84%8B%E1%85%AE%E1%84%80%E1%85%B5-%E1%84%80%E1%85%A1%E1%86%BC%E1%84%8B%E1%85%A1%E1%84%8C%E1%85%B5-%E1%84%8B%E1%85%AD%E1%86%BC%E1%84%91%E1%85%AE%E1%86%B7-%E1%84%80%E1%85%A1%E1%86%BC%E1%84%8B%E1%85%A1%E1%84%8C%E1%85%B5-%E1%84%8F%E1%85%B5%E1%84%8B%E1%85%AE%E1%86%AF-%E1%84%84%E1%85%A2-%E1%84%8C%E1%85%AE%E1%86%AB%E1%84%87%E1%85%B5%E1%84%86%E1%85%AE%E1%86%AF-1.jpg",
+    name: "catmam",
+    text: "책임감이라는 게 생겼어요, 감사합니다",
   },
 ];
 
@@ -60,7 +126,7 @@ const Review = () => {
     <>
       {/* 입양 후기 입니다 */}
       <div>
-        <h2 className="reviewTitle">따뜻한 가족들을 만난 친구들의 이야기</h2>
+        <h2 className="storyTitle">따뜻한 가족들을 만난 친구들의 이야기</h2>
         <div className="Grid">
           {reviews.map((review) => (
             <div className="reviewDiv" key={review.id}>
