508b5440526a25b1e34bfe75815f41f6c3b4fe57
whgmlwls0219 <whgmlwls0219@naver.com>
2025-12-09 01:01:32 +0900

    아이디 찾기 회원정보 (이름o, 전화번호o) 아이디 호출 완료

--- src/assets/components/ShowId.jsx
+++ src/assets/components/ShowId.jsx
@@ -1,40 +1,49 @@
 const ShowId = ({ foundId, foundName, foundTel, goBack }) => {
+  const makeUserIdSecret = (id) => {
+    if (!id) return "";
+    return id.slice(0, id.length - 3) + "***";
+  };
+
+  // 전화번호에 하이픈 추가하는 함수
+  const AddHyphen = (tel) => {
+    if (!tel) return "";
+
+    const onlyNum = tel.replace(/[^0-9]/g, "");
+
+    // 010-XXXX-XXXX 형식
+    if (onlyNum.length === 11) {
+      return onlyNum.replace(/(\d{3})(\d{4})(\d{4})/, "$1-$2-$3");
+    }
+
+    // 그 외 : 그대로 반환
+    return tel;
+  };
+
   return (
     <>
       <div className="found-div">
         <div className="foundTitle-div">
-          <p className="id-completed">아이디 찾기가 완료되었습니다.</p>
+          <p className="idCompleted">아이디 찾기가 완료되었습니다.</p>
           <p className="idNum">
             가입된 아이디가 총{" "}
             <span className="joinedId">{foundId ? 1 : 0}</span>개 있습니다.
           </p>
         </div>
+
+        <div className="grayBorder">
+          <ul className="userInfo-ul">
+            <li className="userName">이&nbsp;&nbsp;름{foundName}</li>
+            <li className="userTel">
+              전&nbsp;화&nbsp;번&nbsp;호&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+              <span>{AddHyphen(foundTel)}</span>
+            </li>
+            <li className="userId">
+              아&nbsp;&nbsp;이&nbsp;&nbsp;디
+              {makeUserIdSecret(foundId)}
+            </li>
+          </ul>
+        </div>
       </div>
-
-      {/* <p className="userId">
-          <strong>{foundId}</strong> <p className="userId-p"></p>
-        </p>
-        <p className="userId">
-          <strong>{foundName}</strong> <p className="userId-p"></p>
-        </p>
-        <p className="userId">
-          <strong>{foundTel}</strong> <p className="userId-p"></p>
-        </p> */}
-
-      {/* <button
-        className="findIdBtn"
-        onClick={goBack}
-        style={{ marginTop: "20px" }}
-      >
-        처음으로
-      </button>
-      <div className="extraMenues-div">
-        <ul className="extraMenues-ul">
-          <li>회원가입&nbsp;|&nbsp;</li>
-          <li>비밀번호 &nbsp;|&nbsp;</li>
-          <li>로그인</li>
-        </ul>
-      </div> */}
     </>
   );
 };
